88f77f5ef73ebee07391d107ed95e9f9f153f5ab
Lucas Brunner <brunnelu@student.ethz.ch>
2019-01-15 21:24:16 +0100

    Initial commit

---
+++ fileupload/test_choices.py
@@ -0,0 +1,29 @@
+m_Jahrgang = (
+   (1,'1'),
+    (2, '2'),
+    (3,'3'),
+    (4,'4'),
+)
+
+m_Fach = (
+    (1, 'Anwendungen der Mathematik'),
+    (2, 'Bildnerisches Gestalten'),
+    (3, 'Biologie'),
+    (4, 'Chemie'),
+    (5, 'Englisch'),
+    (6, 'Ergänzungsfach'),
+    (7, 'Französisch'),
+    (8, 'Geschichte'),
+    (9, 'Italienisch'),
+    (10, 'Latein'),
+    (11, 'Mathematik'),
+    (12, 'Physik'),
+    (13, 'Russisch'),
+    (14, 'Spanisch'),
+    (15, 'Wirtschaft und Recht'),
+)
+
+m_Schwerpunkt = (
+    (1, 'JA'),
+    (2, 'NEIN')
+)
